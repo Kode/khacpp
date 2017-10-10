9d182544c1993f4d2a8dd4e3c5dbc854952843a5
Igor Makaruks <im@Igors-iMac.local>
2017-10-10 21:04:40 +0100

    disabled system() call for compilation on ios11

--- src/hx/libs/std/Sys.cpp
+++ src/hx/libs/std/Sys.cpp
@@ -261,7 +261,7 @@ bool _hx_std_sys_is64()
 **/
 int _hx_std_sys_command( String cmd )
 {
-   #if defined(HX_WINRT) || defined(EMSCRIPTEN) || defined(EPPC) || defined(APPLETV) || defined(HX_APPLEWATCH)
+   #if defined(HX_WINRT) || defined(EMSCRIPTEN) || defined(EPPC) || defined(APPLETV) || defined(HX_APPLEWATCH) || defined(HX_IPHONE)
    return -1;
    #else
    if( !cmd.__s || !cmd.length )
