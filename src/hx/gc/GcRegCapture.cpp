37d0cb15dc8cb29cacf485275add4ddfa83d8e29
Robert Konrad <git2016@robdangero.us>
2021-05-30 20:42:16 +0200

    Revert "Add reg-capture code"
    This reverts commit bc64dcb8ab4fbb97f20ee02d255116609875a424.

--- src/hx/gc/GcRegCapture.cpp
+++ src/hx/gc/GcRegCapture.cpp
@@ -5,169 +5,6 @@
 
 // Nothing
 
-#elif defined(HXCPP_CAPTURE_ARM64)
-
-#pragma optimize( "", off )
-
-namespace hx {
-
-void CaptureARM64(RegisterCaptureBuffer &outBuffer)
-{
-
-   void *regX0;
-   void *regX1;
-   void *regX2;
-   void *regX3;
-   void *regX4;
-   void *regX5;
-   void *regX6;
-   void *regX7;
-   void *regX8;
-   void *regX9;
-   void *regX10;
-   void *regX11;
-   void *regX12;
-   void *regX13;
-   void *regX14;
-   void *regX15;
-   void *regX16;
-   void *regX17;
-   void *regX18;
-   void *regX19;
-   void *regX20;
-   void *regX21;
-   void *regX22;
-   void *regX23;
-   void *regX24;
-   void *regX25;
-   void *regX26;
-   void *regX27;
-   void *regX28;
-   void *regX29;
-asm ("mov %0, x0\n\t" : "=r" (regX0) );
-asm ("mov %0, x1\n\t" : "=r" (regX1) );
-asm ("mov %0, x2\n\t" : "=r" (regX2) );
-asm ("mov %0, x3\n\t" : "=r" (regX3) );
-asm ("mov %0, x4\n\t" : "=r" (regX4) );
-asm ("mov %0, x5\n\t" : "=r" (regX5) );
-asm ("mov %0, x6\n\t" : "=r" (regX6) );
-asm ("mov %0, x7\n\t" : "=r" (regX7) );
-asm ("mov %0, x8\n\t" : "=r" (regX8) );
-asm ("mov %0, x9\n\t" : "=r" (regX9) );
-asm ("mov %0, x10\n\t" : "=r" (regX10) );
-asm ("mov %0, x11\n\t" : "=r" (regX11) );
-asm ("mov %0, x12\n\t" : "=r" (regX12) );
-asm ("mov %0, x13\n\t" : "=r" (regX13) );
-asm ("mov %0, x14\n\t" : "=r" (regX14) );
-asm ("mov %0, x15\n\t" : "=r" (regX15) );
-asm ("mov %0, x16\n\t" : "=r" (regX16) );
-asm ("mov %0, x17\n\t" : "=r" (regX17) );
-asm ("mov %0, x18\n\t" : "=r" (regX18) );
-asm ("mov %0, x19\n\t" : "=r" (regX19) );
-asm ("mov %0, x20\n\t" : "=r" (regX20) );
-asm ("mov %0, x21\n\t" : "=r" (regX21) );
-asm ("mov %0, x22\n\t" : "=r" (regX22) );
-asm ("mov %0, x23\n\t" : "=r" (regX23) );
-asm ("mov %0, x24\n\t" : "=r" (regX24) );
-asm ("mov %0, x25\n\t" : "=r" (regX25) );
-asm ("mov %0, x26\n\t" : "=r" (regX26) );
-asm ("mov %0, x27\n\t" : "=r" (regX27) );
-asm ("mov %0, x28\n\t" : "=r" (regX28) );
-asm ("mov %0, x29\n\t" : "=r" (regX29) );
-  outBuffer.x0 = regX0;
-  outBuffer.x1 = regX1;
-  outBuffer.x2 = regX2;
-  outBuffer.x3 = regX3;
-  outBuffer.x4 = regX4;
-  outBuffer.x5 = regX5;
-  outBuffer.x6 = regX6;
-  outBuffer.x7 = regX7;
-  outBuffer.x8 = regX8;
-  outBuffer.x9 = regX9;
-  outBuffer.x10 = regX10;
-  outBuffer.x11 = regX11;
-  outBuffer.x12 = regX12;
-  outBuffer.x13 = regX13;
-  outBuffer.x14 = regX14;
-  outBuffer.x15 = regX15;
-  outBuffer.x16 = regX16;
-  outBuffer.x17 = regX17;
-  outBuffer.x18 = regX18;
-  outBuffer.x19 = regX19;
-  outBuffer.x20 = regX20;
-  outBuffer.x21 = regX21;
-  outBuffer.x22 = regX22;
-  outBuffer.x23 = regX23;
-  outBuffer.x24 = regX24;
-  outBuffer.x25 = regX25;
-  outBuffer.x26 = regX26;
-  outBuffer.x27 = regX27;
-  outBuffer.x28 = regX28;
-  outBuffer.x29 = regX29;
-}
-
-} // end namespace hx
-
-
-#elif defined(HXCPP_CAPTURE_ARMV7)
-
-#pragma optimize( "", off )
-
-namespace hx {
-
-void CaptureARMV7(RegisterCaptureBuffer &outBuffer)
-{
-
-   void *regR0;
-   void *regR1;
-   void *regR2;
-   void *regR3;
-   void *regR4;
-   void *regR5;
-   void *regR6;
-   void *regR7;
-   void *regR8;
-   void *regR9;
-   void *regR10;
-   void *regR11;
-   void *regR12;
-   void *regR13;
-   void *regR14;
-asm ("mov %0, r0\n\t" : "=r" (regR0) );
-asm ("mov %0, r1\n\t" : "=r" (regR1) );
-asm ("mov %0, r2\n\t" : "=r" (regR2) );
-asm ("mov %0, r3\n\t" : "=r" (regR3) );
-asm ("mov %0, r4\n\t" : "=r" (regR4) );
-asm ("mov %0, r5\n\t" : "=r" (regR5) );
-asm ("mov %0, r6\n\t" : "=r" (regR6) );
-asm ("mov %0, r7\n\t" : "=r" (regR7) );
-asm ("mov %0, r8\n\t" : "=r" (regR8) );
-asm ("mov %0, r9\n\t" : "=r" (regR9) );
-asm ("mov %0, r10\n\t" : "=r" (regR10) );
-asm ("mov %0, r11\n\t" : "=r" (regR11) );
-asm ("mov %0, r12\n\t" : "=r" (regR12) );
-asm ("mov %0, r13\n\t" : "=r" (regR13) );
-asm ("mov %0, r14\n\t" : "=r" (regR14) );
-  outBuffer.r0 = regR0;
-  outBuffer.r1 = regR1;
-  outBuffer.r2 = regR2;
-  outBuffer.r3 = regR3;
-  outBuffer.r4 = regR4;
-  outBuffer.r5 = regR5;
-  outBuffer.r6 = regR6;
-  outBuffer.r7 = regR7;
-  outBuffer.r8 = regR8;
-  outBuffer.r9 = regR9;
-  outBuffer.r10 = regR10;
-  outBuffer.r11 = regR11;
-  outBuffer.r12 = regR12;
-  outBuffer.r13 = regR13;
-  outBuffer.r14 = regR14;
-}
-
-} // end namespace hx
-
-
 #elif defined(HXCPP_CAPTURE_x86) // }  {
 
 #pragma optimize( "", off )
@@ -180,28 +17,20 @@ void CaptureX86(RegisterCaptureBuffer &outBuffer)
    void *regEsi;
    void *regEdi;
    void *regEbx;
-   void *regEax;
-   void *regEcx;
    #ifdef __GNUC__
    asm ("mov %%esi, %0\n\t" : "=r" (regEsi) );
    asm ("mov %%edi, %0\n\t" : "=r" (regEdi) );
    asm ("mov %%ebx, %0\n\t" : "=r" (regEbx) );
-   asm ("mov %%eax, %0\n\t" : "=r" (regEax) );
-   asm ("mov %%ecx, %0\n\t" : "=r" (regEcx) );
    #else
    __asm {
       mov regEsi, esi
       mov regEdi, edi
       mov regEbx, ebx
-      mov regEax, eax
-      mov regEcx, ecx
    }
    #endif
    outBuffer.esi = regEsi;
    outBuffer.edi = regEdi;
    outBuffer.ebx = regEbx;
-   outBuffer.eax = regEax;
-   outBuffer.ecx = regEcx;
 }
 
 } // end namespace hx
--- src/hx/gc/GcRegCapture.h
+++ src/hx/gc/GcRegCapture.h
@@ -1,10 +1,8 @@
 #ifndef HX_GC_HELPERS_INCLUDED
 #define HX_GC_HELPERS_INCLUDED
 
-#if defined(HXCPP_ARM64)
-#define HXCPP_CAPTURE_ARM64
-#elif defined(ANDROID)
-#define HXCPP_CAPTURE_ARMV7
+#if defined(HX_WINDOWS) && defined(HXCPP_ARM64)
+#define HXCPP_CAPTURE_SETJMP
 #endif
 
 #ifdef HXCPP_CAPTURE_SETJMP
@@ -37,89 +35,11 @@ typedef jmp_buf RegisterCaptureBuffer;
 #define CAPTURE_REG_START (int *)(&mRegisterBuf)
 #define CAPTURE_REG_END (int *)(&mRegisterBuf+1)
 
-#elif defined(HXCPP_CAPTURE_ARM64)
-
-
-struct RegisterCaptureBuffer
-{
-  void *x0;
-   void *x1;
-   void *x2;
-   void *x3;
-   void *x4;
-   void *x5;
-   void *x6;
-   void *x7;
-   void *x8;
-   void *x9;
-   void *x10;
-   void *x11;
-   void *x12;
-   void *x13;
-   void *x14;
-   void *x15;
-   void *x16;
-   void *x17;
-   void *x18;
-   void *x19;
-   void *x20;
-   void *x21;
-   void *x22;
-   void *x23;
-   void *x24;
-   void *x25;
-   void *x26;
-   void *x27;
-   void *x28;
-   void *x29;
-};
-
-void CaptureARM64(RegisterCaptureBuffer &outBuffer);
-
-#define CAPTURE_REGS \
-   hx::CaptureARM64(mRegisterBuf);
-
-#define CAPTURE_REG_START (int *)(&mRegisterBuf)
-#define CAPTURE_REG_END (int *)(&mRegisterBuf+1)
-
-
-#elif defined(HXCPP_CAPTURE_ARMV7)
-
-
-struct RegisterCaptureBuffer
-{
-  void *r0;
-   void *r1;
-   void *r2;
-   void *r3;
-   void *r4;
-   void *r5;
-   void *r6;
-   void *r7;
-   void *r8;
-   void *r9;
-   void *r10;
-   void *r11;
-   void *r12;
-   void *r13;
-   void *r14;
-};
-
-void CaptureARMV7(RegisterCaptureBuffer &outBuffer);
-
-#define CAPTURE_REGS \
-   hx::CaptureARMV7(mRegisterBuf);
-
-#define CAPTURE_REG_START (int *)(&mRegisterBuf)
-#define CAPTURE_REG_END (int *)(&mRegisterBuf+1)
-
 #elif defined(HXCPP_CAPTURE_x86) // } {
 
 struct RegisterCaptureBuffer
 {
-   void *eax;
    void *ebx;
-   void *ecx;
    void *edi;
    void *esi;
 };
@@ -157,6 +77,7 @@ void CaptureX64(RegisterCaptureBuffer &outBuffer);
 #define CAPTURE_REG_END (int *)(&mRegisterBuf+1)
 
 #else 
+
 
 class RegisterCapture
 {
