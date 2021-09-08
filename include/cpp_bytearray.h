89d47ff706872e7fac53641de21135f583a0479a
Robert Konrad <git2016@robdangero.us>
2021-09-08 02:16:27 +0200

    Remove unused bytearray things

--- include/cpp_bytearray.h
+++ include/cpp_bytearray.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include <assert.h>
 #include <stdint.h>
 
 struct bytearray {
 	uint8_t *data;
-	int myLength;
 	int refCount;
 
 	bytearray() {
@@ -12,9 +12,8 @@ struct bytearray {
 		refCount = 0;
 	}
 
-	void alloc(int elements) {
-		myLength = elements;
-		data = new uint8_t[myLength];
+	void alloc(int length) {
+		data = new uint8_t[length];
 	}
 
 	void addRef() {
@@ -29,22 +28,11 @@ struct bytearray {
 		}
 	}
 
-	int byteLength() {
-		return myLength;
-	}
-
 	float get(int index) {
 		return data[index];
 	}
 
 	float set(int index, float value) {
 		return data[index] = value;
-	}
-
-	bytearray slice(int begin, int end) {
-		bytearray arr;
-		arr.data = &data[begin];
-		arr.myLength = end - begin;
-		return arr;
 	}
 };
